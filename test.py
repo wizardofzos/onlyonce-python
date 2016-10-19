9c56ccddcf79ea66e9338cf3da8c7b58e79ba6be
Henri Kuiper <wizard@zdevops.com>
2016-10-19 21:02:24 +0200

    testcase

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from oopyconnector import OO, OOCardParser
 
 import datetime
 import pprint
@@ -8,7 +7,6 @@ pp = pprint.PrettyPrinter(indent=4)
 seckey = "your_very_secret_key"
 o = OO()
 
-o.baseurl='https://api.onlyonce.com'
 
 print "              _                            "
 print "   ___  _ __ | |_   _  ___  _ __   ___ ___ "
@@ -19,40 +17,18 @@ print "                |___/                      "
 print ""
 print " Command line demonstration of the OO-API  "
 print ""
-print "Your API-Key   :",
-o.apikey=raw_input()
-print "Your API-Secret",
-o.apisec=raw_input()
-o.register()
-print " - registered at OOAPI and received bearer token"
-cards = o.cards()
-print " - fetch all cards shared with me"
 
-keepgoing = True
+import oopyconnector
+
+o = OO()
 
 
-while keepgoing:
-    pos = 0
-    for card in cards:
-        print "%s - %s" % (pos, card['name'])
-        pos += 1
-
-    print "What card to print, X for exit",
-    p = raw_input()
-    if p == 'X':
-        keepgoing = False
-    else:
-        p = int(p)
-        print "Need SecKey (not shown)",
-        seckey = getpass.getpass()
+o.baseurl='https://api.onlyonce.com'
+o.username = os.environ.get('oouser')
+o.password = os.environ.get('oopass')
 
 
-        c = o.card(cards[p]['id'],seckey)
-        print "-------------------- FULL JSON OF CARD DATA ----------------"
-        pp.pprint(c)
-        parser = OOCardParser()
-        print "-------------------- BASIC INFO FROM CARD (IF ANY)----------"
-        print parser.getInfo(c['data']['model'][0],['company_street_name_field'])
-        print "<Enter> to continue"
-        a = raw_input()
-
+o.signin()
+profiles = o.profiles()
+for profile in profiles:
+      all_cards = o.cards(profile=profile['id'])
